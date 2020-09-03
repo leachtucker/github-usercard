4254a8dd5c00dccf72f3e08c515b970b252cb250
Tucker Leach <leachtucker@gmail.com>
2020-09-03 14:43:35 -0700

    Finished MVP. Beginning to refactor code

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,60 +1,92 @@
-/*
-  STEP 1: using axios, send a GET request to the following URL
-    (replacing the placeholder with your Github name):
-    https://api.github.com/users/<your name>
-*/
+// Import
+import axios from 'axios';
 
-/*
-  STEP 2: Inspect and study the data coming back, this is YOUR
-    github info! You will need to understand the structure of this
-    data in order to use it to build your component function
+// Const
+const cardsContainer = document.querySelector('div.cards');
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
-    Skip to STEP 3.
-*/
+// >>>>>>>>>> Used httpie to review the data passed through the response
 
-/*
-  STEP 4: Pass the data received from Github into your function,
-    and append the returned markup to the DOM as a child of .cards
-*/
+// Request's userdata for given username and then creates & appends a card to the DOM with data derived from a GET response
+function getUser(username) {
+    return axios.get(`https://api.github.com/users/${username}`)
+        .then(response => {
+            // Success -- create and append the card derived from the user data we recieved
+            const userData = response.data;
+            const newCard = cardMaker(userData);
+            cardsContainer.append(newCard);
+        })
+        .catch(err => {
+            console.log('There has been an error loading user data')
+        });
+}
 
-/*
-  STEP 5: Now that you have your own card getting added to the DOM, either
-    follow this link in your browser https://api.github.com/users/<Your github name>/followers,
-    manually find some other users' github handles, or use the list found at the
-    bottom of the page. Get at least 5 different Github usernames and add them as
-    Individual strings to the friendsArray below.
+// 1. Request my user data
+getUser('leachtucker');
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
-*/
+// 2. Request user data for each username in the array
+followersArray.forEach(username => {
+    getUser(username);
+});
 
-const followersArray = [];
+// 3. Stretch -- Request the user's followers' data
+function getFollowers(user) {
+    axios.get(user["followers_url"])
+        .then(response => {
+            // Success -- iterate through the data we recieved. Create & append a card for each passthrough
+            response.data.forEach(follower => {
+                console.log(follower);
+                cardsContainer.append(cardMaker(follower));
+            })
+        })
+        .catch(err => {
+            console.log(`An error has occured retrieving the users followers: ${err}`);
+        });
+}
 
-/*
-  STEP 3: Create a function that accepts a single object as its only argument.
-    Using DOM methods and properties, create and return the following markup:
 
-    <div class="card">
-      <img src={image url of user} />
-      <div class="card-info">
-        <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
-        <p>Location: {users location}</p>
-        <p>Profile:
-          <a href={address to users github page}>{address to users github page}</a>
-        </p>
-        <p>Followers: {users followers count}</p>
-        <p>Following: {users following count}</p>
-        <p>Bio: {users bio}</p>
-      </div>
-    </div>
-*/
+// Component
+function cardMaker(user) {
+    // Setting up the div.card element (the outer most element)
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-/*
-  List of LS Instructors Github username's:
-    tetondan
-    dustinmyers
-    justsml
-    luishrd
-    bigknell
-*/
+    // Setting up sub elements
+    const avatarImg = document.createElement('img');
+    avatarImg.setAttribute('src', user["avatar_url"]);
+
+    const infoDiv = document.createElement('div');
+    infoDiv.classList.add('card-info');
+
+    const nameH3 = document.createElement('h3');
+    nameH3.classList.add('name');
+    nameH3.textContent = user["name"];
+
+    const usernameP = document.createElement('p');
+    usernameP.classList.add('username');
+    usernameP.textContent = user["login"];
+
+    const locationP = document.createElement('p');
+    locationP.textContent = user["location"];
+
+    const profileP = document.createElement('p');
+    const profileLink = document.createElement('a');
+    profileP.appendChild(profileLink);
+    profileLink.setAttribute('src', user["url"]);
+
+    const followersP = document.createElement('p');
+    followersP.textContent = user["followers"];
+
+    const followingP = document.createElement('p');
+    followingP.textContent = user["following"];
+
+    const bioP = document.createElement('p');
+    bioP.textContent = user["bio"];
+
+    // Appending sub elements to their parent elements/containers
+    infoDiv.append(nameH3, usernameP, locationP, profileP, followersP, followingP, bioP);
+    card.append(avatarImg, infoDiv);
+
+    // Return the created div.card
+    return card;
+}
